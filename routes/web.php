cbf43dfb283c1e7171e7791b5a7fe1a34faf4c08
Kennedy Mutuku <kenmsimba@gmail.com>
2025-11-01 23:10:54 +0300

    Added a test route & page to test vue

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home'); //Matches pages/Home.vue
 });
 
 Route::get('/test', fn() => Inertia::render('Test'));
